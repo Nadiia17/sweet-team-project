ed3cf163aad90e62479081d8d32e8d940322a336
ArtSatanov <artsatanov@gmail.com>
2023-06-04 03:55:04 +0300

    fix

---
+++ src/js/slick-reviews.js
@@ -0,0 +1,32 @@
+$('.reviews-container').slick({
+  dots: true,
+  infinite: false,
+  arrows: false,
+  speed: 250,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  draggable: true,
+  variableWidth: false,
+  responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        variableWidth: true,
+        dots: true,
+      },
+    },
+
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        swipe: true,
+        variableWidth: false,
+      },
+    },
+  ],
+});
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import './js/slick';
 // import './js/modal';
 import './js/modal-reviews';
+import './js/slick-reviews';
