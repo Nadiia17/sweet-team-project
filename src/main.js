2d5997916b6bb9c0c033dc036aa4c49892c76665
ArtSatanov <artsatanov@gmail.com>
2023-06-05 00:48:07 +0300

    modal script fix

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import './js/slick';
 import './js/mobile-menu';
 import './js/modal-sellers';
 import './js/modal-hero';
-import './js/modal';
+// import './js/modal';
 import './js/slick-reviews';
 import './js/modal-reviews';
