e11fd74aad6beb5514e1c78cbaefa0cf4630c42e
Bogdan Protasenko <b.protasenko@gmail.com>
2023-06-05 09:15:04 +0200

    fixed scripts modal-sellers and modal-hero

--- src/js/modal-hero.js
+++ src/js/modal-hero.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-hero]"),
-    closeModalBtn: document.querySelector("[data-modal-close-hero]"),
-    modal: document.querySelector("[data-modal-hero]"),
+    openModalBtn: document.querySelector('[data-modal-open-hero]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal-hero]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    refs.modal.classList.toggle('is-hidden');
   }
 })();
--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/slick';
 import './js/mobile-menu';
-import './js/modal-sellers';
 import './js/modal-hero';
+import './js/modal-sellers';
 // import './js/modal';
 import './js/slick-reviews';
 import './js/modal-reviews';
